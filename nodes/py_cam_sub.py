44138ffcaa1995663e3f39388e118eed55eef86f
Nicholas Paul <npaul@ltu.edu>
2018-05-07 14:43:00 -0400

    clean up python image subscriber

--- nodes/py_cam_sub.py
+++ nodes/py_cam_sub.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
-"""OpenCV feature detectors with ros CompressedImage Topics in python.
 
-This example subscribes to a ros topic containing sensor_msgs 
-CompressedImage. It converts the CompressedImage into a numpy.ndarray, 
-then detects and marks features in that image. It finally displays 
-and publishes the new image - again as CompressedImage topic.
-"""
-__author__ =  'Simon Haller <simon.haller at uibk.ac.at>'
-__version__=  '0.1'
-__license__ = 'BSD'
 # Python libs
 import sys, time
 
@@ -29,17 +20,13 @@ from sensor_msgs.msg import CompressedImage
 # from cv_bridge import CvBridge, CvBridgeError
 
 VERBOSE=False
+DISPLAY_IMG=False
 CAM_SUB='/cam_pub/image_raw/compressed'
 
 class image_feature:
 
     def __init__(self):
-        '''Initialize ros publisher, ros subscriber'''
-        # topic where we publish
-        self.image_pub = rospy.Publisher("/output/image_raw/compressed",
-            CompressedImage)
-        # self.bridge = CvBridge()
-
+        '''Initialize ros subscriber'''
         # subscribed Topic
         self.subscriber = rospy.Subscriber(CAM_SUB,
             CompressedImage, self.callback,  queue_size = 1)
@@ -48,47 +35,17 @@ class image_feature:
 
 
     def callback(self, ros_data):
-        '''Callback function of subscribed topic. 
-        Here images get converted and features detected'''
+        '''Callback function of subscribed topic.'''
         if VERBOSE :
             print 'received image of type: "%s"' % ros_data.format
 
         #### direct conversion to CV2 ####
         np_arr = np.fromstring(ros_data.data, np.uint8)
-        #image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
-        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
-        
-        #### Feature detectors using CV2 #### 
-        # "","Grid","Pyramid" + 
-        # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
-        method = "GridFAST"
-        feat_det = cv2.KAZE_create()
-        time1 = time.time()
+        image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-        # convert np image to grayscale
-        featPoints = feat_det.detect(
-            cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY))
-        time2 = time.time()
-        if VERBOSE :
-            print '%s detector found: %s points in: %s sec.'%(method,
-                len(featPoints),time2-time1)
-
-        for featpoint in featPoints:
-            x,y = featpoint.pt
-            cv2.circle(image_np,(int(x),int(y)), 3, (0,0,255), -1)
-        
-        cv2.imshow('cv_img', image_np)
-        cv2.waitKey(2)
-
-        #### Create CompressedIamge ####
-        msg = CompressedImage()
-        msg.header.stamp = rospy.Time.now()
-        msg.format = "jpeg"
-        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
-        # Publish new image
-        self.image_pub.publish(msg)
-        
-        #self.subscriber.unregister()
+        if DISPLAY_IMG:
+            cv2.imshow('cv_img', image_np)
+            cv2.waitKey(2)
 
 def main(args):
     '''Initializes and cleanup ros node'''
@@ -98,7 +55,7 @@ def main(args):
         rospy.spin()
     except KeyboardInterrupt:
         print "Shutting down ROS Image feature detector module"
-    cv2.destroyAllWindows()
+    if DISPLAY_IMG: cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
